5927387dddbccf4cff7331c2cf3f6c82ce95fc21
Karsten <karstenseijkens@gmail.com>
2024-04-12 10:35:20 +0200

    First push

---
+++ Blackjack-Karsten2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Blackjack_Karsten2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
